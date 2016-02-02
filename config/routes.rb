70af25411dafa44ce10733c0bb1b73fd18636b54
jgthibodeau <jgthibodeau@umass.edu>
2016-02-02 14:54:23 -0500

    errors

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   # Home Pages -------------------------------------------------------
   resources :home, only: [:index]
+
+  #Errors --------------------------------------------------------------
+  get 'file_not_found' => 'application#render_404', as: 'file_not_found'
+  match '/:anything', to: "application#render_404", constraints: { anything: /.*/ }, via: [:get, :post]
 end
