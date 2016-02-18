60a4b60974b5a88294051d8cc23b3fa902f73540
jgthibodeau <jgthibodeau@umass.edu>
2016-02-18 15:25:41 -0500

    setting up factory girl

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "has a valid factory" do
-    create(:contact).should be_valid
+    create(:user).should be_valid
   end
-
-  it "is invalid without a firstname"
-  it "is invalid without a lastname"
-  it "is invalid without an email"
-  it "is invalid without a phone"
-  it "is invalid without a spire_id"
-
+  it "is invalid without a first_name" do
+    build(:user, first_name: nil).should_not be_valid
+  end
+  it "is invalid without a last_name" do
+    build(:user, last_name: nil).should_not be_valid
+  end
+  it "is invalid without an email" do
+    build(:user, email: nil).should_not be_valid
+  end
+  it "is invalid without a phone" do
+    build(:user, phone: nil).should_not be_valid
+  end
+  it "is invalid without a spire_id" do
+    build(:user, spire_id: nil).should_not be_valid
+  end
+  
+  describe ''
   it "returns a contact's full name as a string"
 end
