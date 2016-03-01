1d1da800cd25913c43155fc2e28471f70dcb73b4
Tom Peck <tapeck@umass.edu>
2016-03-01 12:23:08 -0500

    moar validations! and a get fee method

--- app/models/incidental_type.rb
+++ app/models/incidental_type.rb
@@ -3,6 +3,9 @@ class IncidentalType < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :base
+  validates :base, numericality: {greater_than_or_equal_to: 0}
   validates_presence_of :modifier_amount
+  validates :modifier_amount, numericality: {greater_than_or_equal_to: 0}
   validates_presence_of :modifier_description
+
 end
--- app/models/incurred_incidental.rb
+++ app/models/incurred_incidental.rb
@@ -2,6 +2,11 @@ class IncurredIncidental < ActiveRecord::Base
   belongs_to :rental
   has_one :incidental_type
   validates_presence_of :times_modified
+
   validates_presence_of :notes
   validates_associated :incidental_type
+
+  def get_fee
+    incidental_type.base + (times_modified * incidental_type.modifier_amount)
+  end
 end
