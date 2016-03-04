6bd49e765072af3004710c791d77ac171e7f4bb0
Tom Peck <tapeck@umass.edu>
2016-03-04 15:54:25 -0500

    update style

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,7 +1,7 @@
 require 'json'
 class Inventory < ActiveRecord::Base
-#this class is all mocked for now
-#in the future it should raise an exception if the api doesnt return a sucess status
+  # this class is all mocked for now
+  # in the future it should raise an exception if the api doesnt return a sucess status
 
   def self.item_types
     JSON.parse('[{"id": 100, "name": "Apples",
@@ -17,19 +17,19 @@ class Inventory < ActiveRecord::Base
                 {\"id\": 401, \"name\": \"Granny Smith\"}]}")
   end
 
-  def self.update_item_type(uuid, key, value)
-    JSON.parse('[{"id": 100, "name": "Apples",
-                "allowed_keys": ["flavor"],
-                "items": [{"name": "Macintosh"},
-                          {"name": "Granny Smith"}]}]')
+  def self.update_item_type(uuid, _key, _value)
+    JSON.parse("[{\"id\": #{uuid}, \"name\": \"Apples\",
+                \"allowed_keys\": [\"flavor\"],
+                \"items\": [{\"name\": \"Macintosh\"},
+                            {\"name\": \"Granny Smith\"}]}]")
   end
 
-  def self.delete_item_type(uuid)
-    #returns nothing on success
+  def self.delete_item_type(_uuid)
+    # returns nothing on success
   end
 
   def self.create_item_type(name, allowed_keys = [])
-    JSON.parse("{\"id\": #{SecureRandom.uuid}, \"name\": \"#{name}\", \"allowed_keys\": #{allowed_keys.to_s},
+    JSON.parse("{\"id\": #{SecureRandom.uuid}, \"name\": \"#{name}\", \"allowed_keys\": #{allowed_keys},
                 \"items\": []}")
   end
 
@@ -46,12 +46,12 @@ class Inventory < ActiveRecord::Base
     JSON.parse("{\"id\": #{uuid}, \"name\": \"Awesome new couch\", \"item_type_id\": 101, \"data\": {}}")
   end
 
-  def self.update_item(uuid, key, value)
+  def self.update_item(uuid, _key, _value)
     JSON.parse("{\"id\": #{uuid}, \"name\": \"Awesome new couch\", \"item_type_id\": 101, \"data\": {}}")
   end
 
-  def self.delete_item(uuid)
-    #returns nothing on success
+  def self.delete_item(_uuid)
+    # returns nothing on success
   end
 
   def self.create_reservation(item_type, start_time, end_time)
@@ -62,33 +62,33 @@ class Inventory < ActiveRecord::Base
     \"item\": \"Dummy Data\"}")
   end
 
-  #maybe in the future add a few helper methods like update_start_time(uuid,start_time)
-  def self.update_reservation(uuid, key, value)
+  # maybe in the future add a few helper methods like update_start_time(uuid,start_time)
+  def self.update_reservation(uuid, _key, _value)
     JSON.parse("{\"id\": #{uuid},
-    \"start_time\": \"#{value}\",
+    \"start_time\": \"2016-02-16T15:30:00-05:00\",
     \"end_time\": \"2016-02-16T18:00:00-05:00\",
     \"item_type\": \"Apple\",
     \"item\": \"Dummy Data\"}")
   end
 
   def self.reservation(uuid)
-    JSON.parse('{"id": 100,
-    "start_time": "2016-02-16T15:30:00-05:00",
-    "end_time": "2016-02-17T09:45:00-05:00",
-    "item_type": "Apples",
-    "item": "Granny Smith"}')
+    JSON.parse("{\"id\": #{uuid},
+    \"start_time\": \"2016-02-16T15:30:00-05:00\",
+    \"end_time\": \"2016-02-17T09:45:00-05:00\",
+    \"item_type\": \"Apples\",
+    \"item\": \"Granny Smith\"}")
   end
 
-  def self.delete_reservation(uuid)
-    #returns nothing on success
+  def self.delete_reservation(_uuid)
+    # returns nothing on success
   end
   
-  def self.update_reservation_item(key, value)
-    #returns nothing
+  def self.update_reservation(_key, _value)
+    # returns nothing
   end
 
-  #is this distinct enough from the singular method?
-  def self.reservations(start_time = 100.years.ago,end_time = 100.years.from_now, item_type)
+  # is this distinct enough from the singular method?
+  def self.reservations(_start_time = 100.years.ago, _end_time = 100.years.from_now, _item_type)
     JSON.parse('[{"start_time": "2016-02-11T15:45:00-05:00", "end_time": "2016-02-11T21:00:00-05:00"},
                 {"start_time": "2016-02-17T10:30:00-05:00", "end_time": "2016-02-19T21:00:00-05:00"}]')
   end
