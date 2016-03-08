a157d2a308f75904643e45b7bdee1398ef0044d2
Tom Peck <tapeck@umass.edu>
2016-03-08 12:33:12 -0500

    one more spec

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
+  before :all do # this is only for the mocks!
+    @uuid = 'not a real uuid'
+  end
+
   context 'item types' do
     it 'returns a array of types' do
       array = Inventory.item_types
       expect(array).to be_a(Array)
     end
 
-    it 'returns the right item type' do
-      #should there be some type of factory?
+    it 'returns the one item given a uuid' do
+      expect(Inventory.item_type(@uuid)).to be_a(Hash)
     end
   end
 end
