0ed9d86db37284f65cdaaa9d00a0e2549d749f5b
Tom Peck <tapeck@umass.edu>
2016-03-15 12:15:20 -0400

    set up secret api keys

--- config/initializers/inventory_api_secrets.rb
+++ config/initializers/inventory_api_secrets.rb
@@ -1,7 +1,7 @@
-yaml = 'config/inventory_api_keys.yaml'
+yaml = 'config/inventory_api_keys.yml'
 if File.exist?(yaml)
   require "erb"
   all_secrets = YAML.load(ERB.new(IO.read(yaml)).result) || {}
   env_secrets = all_secrets[Rails.env]
-  secrets.merge!(env_secrets.symbolize_keys) if env_secrets
+  INVENTORY_API_KEY = env_secrets.symbolize_keys[:inventory_api_key] if env_secrets
 end
