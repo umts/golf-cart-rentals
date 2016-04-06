ca04c5cc37fcec96b974bc0b8437db7f833a461f
Alfonso Merkl <anmerkl@gmail.com>
2016-04-06 12:57:52 -0400

    finished basic functionality for incurred incidentals. updated seeds to actually work now. updated views with all basic functionality

--- app/controllers/incurred_incidentals_controller.rb
+++ app/controllers/incurred_incidentals_controller.rb
@@ -1,11 +1,12 @@
 class IncurredIncidentalsController < ApplicationController
-  before_action :get_rental_incidental
+  before_action :get_rental
 
   def index
     @incidentals = @rental.incurred_incidentals
   end
 
   def show
+    @incidental = IncurredIncidental.find(params[:id])
   end
 
   def new
@@ -13,14 +14,30 @@ class IncurredIncidentalsController < ApplicationController
   end
 
   def create
-    @incidental = @rental.incurred_incidentals.create(incidental_params)
+    @incidental = @rental.incurred_incidentals.new(incidental_params)
+    respond_to do |format|
+      if @incidental.save
+        format.html { redirect_to rental_incurred_incidental_path(@rental, @incidental), notice: 'Incidental successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
+    @incidental = IncurredIncidental.find(params[:id])
   end
 
   def update
-    @incidental.update(incidental_params)
+    @incidental = IncurredIncidental.find(params[:id])
+
+    respond_to do |format|
+      if @incidental.update(incidental_params)
+        format.html { redirect_to rental_incurred_incidental_path(@rental, @incidental), notice: 'Incidental successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def destroy
@@ -29,9 +46,8 @@ class IncurredIncidentalsController < ApplicationController
 
   private
 
-    def get_rental_incidental
+    def get_rental
       @rental = Rental.find(params[:rental_id])
-      @incidental = IncurredIncidental.find(params[:id])
     end
 
     def incidental_params
