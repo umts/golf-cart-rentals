e15e31f1c9cd200167dc9fdf4b5618636a8008dd
Oscar Lee <owlee@umass.edu>
2016-04-13 12:43:36 -0400

    Much sleeker code in Factory Girl to use traits and nested factories (w/ polymorphic association)

--- spec/factories/financial_transaction.rb
+++ spec/factories/financial_transaction.rb
@@ -1,24 +1,27 @@
 FactoryGirl.define do
-
   factory :financial_transaction do
     rental
-    #association :rental, factory: :rental_id
-    #rental_id { create(:rental).id }
-    #association :rental, factory: :transactable_id
-    #transactable_id
-    #transactable_type "Rental"
     amount 100
     adjustment 0
+    note_field "Property of Factory Girl"
     created_at DateTime.now
+    updated_at DateTime.now
+
+    trait :with_transactable_incidental do
+      association :transactable, factory: :incidental_type
+    end
+
+    trait :with_transactable_fee do
+      association :transactable, factory: :fee_schedule
+    end
 
     factory :incidental_type_transaction do
-      transactable_type 'IncidentalType'
-      #transactable_id 1
+      with_transactable_incidental
     end
 
     factory :fee_schedule_transaction do
-      transactable_type 'FeeSchedule'
-      #transactable_id 1
+      with_transactable_fee
     end
   end
 end
+
--- spec/models/financial_transaction_spec.rb
+++ spec/models/financial_transaction_spec.rb
@@ -1,33 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe FinancialTransaction, type: :model do
-    it 'creates a valid financial transcation after creating a rental' do
-      rental = create(:rental)
-      f1 = build(:financial_transaction, rental_id: rental.id)
-      expect(f1.rental).to eq(rental)
-      #expect(rental.financial_transactions.include? f1.rental).to eq(true)
-      #expect(f1).to belong_to(:rental)
-    end
+  it 'creates a valid financial transaction after creating a rental' do
+    #binding.pry
+    rental = create(:rental)
+    modified_transaction = build(:financial_transaction, rental_id: rental.id)
+    unmodified_transaction = build(:financial_transaction)
 
-    it 'creates a valid financial transcation after creating an incurred incidental' do
-      binding.pry
-      rental = create(:rental)
-      incidental = create(:incidental_type)
-      financial_transaction = build(:incidental_type_transaction,
-                                    rental_id: rental.id,
-                                    transactable_id: incidental.id
-                                   )
-      expect(financial_transaction.transactable).to eq(incidental)
-    end
+    expect(modified_transaction).to be_valid
+    expect(unmodified_transaction).to be_valid
 
-    it 'creates a valid financial transcation after creating an fee schedule' do
-      binding.pry
-      rental = create(:rental)
-      fee_schedule = create(:fee_schedule)
-      financial_transaction = build(:fee_schedule_transaction,
-                                    rental_id: rental.id,
-                                    transactable_id: fee_schedule.id
-                                   )
-      expect(financial_transaction.transactable).to eq(fee_schedule)
-    end
+    expect(modified_transaction.rental).to eq(rental)
+    expect(unmodified_transaction.rental).to be_valid
+
+    #expect(rental.financial_transactions.include? f1.rental).to eq(true)
+    #expect(f1).to belong_to(:rental)
+  end
+
+  it 'creates a valid financial transaction after creating an incurred incidental' do
+    #binding.pry
+
+    incidental = create(:incidental_type_transaction)
+    unmodified_transaction = build(:incidental_type_transaction)
+    modified_transaction =
+
+    financial_transaction = build(:incidental_type_transaction,
+                                  rental_id: rental.id,
+                                  transactable_id: incidental.id
+    )
+    expect(financial_transaction.transactable).to eq(incidental)
+  end
+
+  it 'creates a valid financial transcation after creating an fee schedule' do
+    #binding.pry
+    rental = create(:rental)
+    fee_schedule = create(:fee_schedule)
+    financial_transaction = build(:fee_schedule_transaction,
+                                  rental_id: rental.id,
+                                  transactable_id: fee_schedule.id
+    )
+    expect(financial_transaction.transactable).to eq(fee_schedule)
+  end
 end
