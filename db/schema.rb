51c7b9920bd2f3625d921e5740741ef0aeff3682
Tom Peck <tapeck@umass.edu>
2016-05-16 15:31:41 -0400

    trying to get code merged properly

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160504183330) do
-
-  create_table "departments", force: :cascade do |t|
-    t.string   "name",       limit: 255,                null: false
-    t.boolean  "active",                 default: true, null: false
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-  end
-
   create_table "departments", force: :cascade do |t|
     t.string   "name",       limit: 255,                null: false
     t.integer  "user_id",    limit: 4,                  null: false
@@ -27,8 +19,6 @@ ActiveRecord::Schema.define(version: 20160504183330) do
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
   end
-
-  add_index "departments", ["user_id"], name: "index_departments_on_user_id", using: :btree
 
   create_table "documents", force: :cascade do |t|
     t.string   "filename",   limit: 255, null: false
