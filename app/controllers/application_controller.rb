2ea04b16e7a7ada8c6ed751af7fc8a7e62ac4f4e
Tom Peck <tapeck@umass.edu>
2016-05-16 15:40:59 -0400

    sdg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+class MissingUserError < StandardError; end
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
