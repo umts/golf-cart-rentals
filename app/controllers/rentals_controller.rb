f3bdd6932d7157b74624a082780e9c4b3c984eb8
Oscar Lee <owlee@umass.edu>
2016-07-15 09:22:28 -0400

    Fix for couldnt find User id.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -69,7 +69,7 @@ class RentalsController < ApplicationController
   def create
     @rental = Rental.new(rental_params)
 
-    @start_date = param['start_date'] ? param['start_date'] : Time.zone.today
+    @start_date = params['start_date'] ? params['start_date'] : Time.zone.today
 
     if @rental.save
       flash[:success] = 'You have succesfully reserved your Rental!'
