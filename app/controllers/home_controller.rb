2f6897c634f428c7972681f33b2e488743cd9906
Charles Mednieks <cmednieks@umass.edu>
2016-07-20 12:03:12 -0400

    fixing codeclimate style issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,13 @@ class HomeController < ApplicationController
   def index
     @rentals = Rental.all
     @item_types = ItemType.all
-    @upcoming_rentals = @rentals.where('end_time > ? AND rental_status = ?', DateTime.now, "reserved")
-    @ongoing_rentals = @rentals.where('rental_status = ?', "checked_out")
-    @no_show_rentals = @rentals.where('end_time <= ? AND rental_status = ?', DateTime.now, "reserved")
-    @q = @rentals.where('rental_status = ?', "checked_in").search(params[:q])
+    @upcoming_rentals = @rentals.where('end_time > ? AND rental_status = ?', DateTime.current, 'reserved')
+    @ongoing_rentals = @rentals.where('rental_status = ?', 'checked_out')
+    @no_show_rentals = @rentals.where('end_time <= ? AND rental_status = ?', DateTime.current, 'reserved')
+    @q = @rentals.where('rental_status = ?', 'checked_in').search(params[:q])
     @past_rentals = @q.result(distinct: true).paginate(page: params[:page], per_page: @per_page)
-    
-    unless @current_user.groups.find_by(name: "admin").present?
+
+    unless @current_user.groups.find_by(name: 'admin').present?
       @upcoming_rentals = @upcoming_rentals.where('user_id = ?', @current_user.id)
       @ongoing_rentals = @ongoing_rentals.where('user_id = ?', @current_user.id)
       @no_show_rentals = @no_show_rentals.where('user_id = ?', @current_user.id)
