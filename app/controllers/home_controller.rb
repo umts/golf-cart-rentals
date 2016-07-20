f45f07e6d03f6345cb6f70c92d9bdae75e6b8f1e
Charles Mednieks <cmednieks@umass.edu>
2016-07-20 15:07:50 -0400

    codeclimate fixed for real this time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,23 +7,22 @@ class HomeController < ApplicationController
     categorize_rentals
     non_admin_filter unless @current_user.groups.find_by(name: 'admin').present?
   end
-  
+
   private
-  
-    def categorize_rentals
-      @upcoming_rentals = @rentals.where('end_time > ? AND rental_status = ?', DateTime.current, 'reserved')
-      @ongoing_rentals = @rentals.where('rental_status = ?', 'checked_out')
-      @no_show_rentals = @rentals.where('end_time <= ? AND rental_status = ?', DateTime.current, 'reserved')
-      @q = @rentals.where('rental_status = ?', 'checked_in').search(params[:q])
-      @past_rentals = @q.result(distinct: true).paginate(page: params[:page], per_page: @per_page)
-    end
-  
-    def non_admin_filter
-      @upcoming_rentals = @upcoming_rentals.where('user_id = ?', @current_user.id)
-      @ongoing_rentals = @ongoing_rentals.where('user_id = ?', @current_user.id)
-      @no_show_rentals = @no_show_rentals.where('user_id = ?', @current_user.id)
-      @q = @q.where('user_id = ?', @current_user.id).search(params[:q])
-      @past_rentals = @q.result(distinct: true).paginate(page: params[:page], per_page: @per_page)
-    end
-  
+
+  def categorize_rentals
+    @upcoming_rentals = @rentals.where('end_time > ? AND rental_status = ?', DateTime.current, 'reserved')
+    @ongoing_rentals = @rentals.where('rental_status = ?', 'checked_out')
+    @no_show_rentals = @rentals.where('end_time <= ? AND rental_status = ?', DateTime.current, 'reserved')
+    @q = @rentals.where('rental_status = ?', 'checked_in').search(params[:q])
+    @past_rentals = @q.result(distinct: true).paginate(page: params[:page], per_page: @per_page)
+  end
+
+  def non_admin_filter
+    @upcoming_rentals = @upcoming_rentals.where('user_id = ?', @current_user.id)
+    @ongoing_rentals = @ongoing_rentals.where('user_id = ?', @current_user.id)
+    @no_show_rentals = @no_show_rentals.where('user_id = ?', @current_user.id)
+    @q = @q.where('user_id = ?', @current_user.id).search(params[:q])
+    @past_rentals = @q.result(distinct: true).paginate(page: params[:page], per_page: @per_page)
+  end  
 end
