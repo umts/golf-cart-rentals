782309f3a46511c6c18011d32d96a52d7e5cddee
Oscar Lee <owlee@umass.edu>
2016-07-22 13:46:30 -0400

    Forms + Disclaimer check

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,5 +1,4 @@
-//Clear search fields
-//Add <%= f.submit "Clear", :name => nil, :id => :q_reset %> to the bottom of your search form
+//Clear search fields //Add <%= f.submit "Clear", :name => nil, :id => :q_reset %> to the bottom of your search form
 $(function(){
   $("#q_reset").click(function(index, element){
     $("input,select").not(':submit').val('')
@@ -43,6 +42,9 @@ function toggle_loading(){
 function accept_and_enable(box)
 {
     var submit = document.getElementById("TOC");
+    alert($(box));
+    console.log($(box).parent().parent().parent()[0]);
+
     if (box.checked === true)
     {
         submit.disabled = false;
