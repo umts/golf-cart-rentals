b2cadc85e7bd983dfa8c72ae1a47b1c436dd1ad5
Oscar Lee <owlee@umass.edu>
2016-07-26 14:38:45 -0400

    Nit picking on spacing

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -70,7 +70,7 @@ class RentalsController < ApplicationController
   def create
     @rental = Rental.new(rental_params)
 
-    @start_date = params['start_date']  || Time.zone.today
+    @start_date = params['start_date'] || Time.zone.today
 
     if @rental.save
       flash[:success] = 'You have succesfully reserved your Rental!'
