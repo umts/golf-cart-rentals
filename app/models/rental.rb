5a3cd74ef7b7625e826a303226aaab6b201d3435
Ciaran <ciaranobrien@umass.edu>
2016-07-27 16:14:37 -0400

    Fixed back buttons on rental form and fixed Rspec

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -70,7 +70,7 @@ class Rental < ActiveRecord::Base
     begin
       reservation = Inventory.create_reservation(item_type.name, start_time, end_time)
       self.reservation_id = reservation[:uuid]
-      self.item_id = Item.find_by(name: reservation['item']).id
+      self.item = Item.find_by(name: reservation[:item])
     rescue => error
       errors.add :base, error.inspect
       return false
