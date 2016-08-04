c0ecc04a0ecf206b100cc4d5d91e9587153460ad
G. Logan Slinski <phantom.patrol@gmail.com>
2016-08-04 11:19:28 -0400

    Adding additional item specs, for .deleted method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,13 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-	it 'has a valid factory' do
-	  expect(build(:item)).to be_valid
-	end
-	it 'is invalid without a name' do
-	  expect(build(:item, name: "")).not_to be_valid
-	end
-	it 'is invalid without an item type' do
-		expect(build(:item, item_type_id: nil)).not_to be_valid
-	end
+  it 'has a valid factory' do
+    expect(build(:item)).to be_valid
+  end
+  it 'is invalid without a name' do
+    expect(build(:item, name: "")).not_to be_valid
+  end
+  it 'is invalid without an item type' do
+    expect(build(:item, item_type_id: nil)).not_to be_valid
+  end
+
+  describe '.deleted' do
+    it 'returns an empty collection when no records are present' do
+      expect(Item.deleted).to be_empty
+    end
+
+    it 'returns an empty collection, when no deleted items exist' do
+      create(:item)
+      expect(Item.deleted).to be_empty
+    end
+
+    it 'returns a collection with records when one deleted item exists' do
+      item = create(:item) and item.destroy
+      expect(Item.deleted).to include(item)
+    end
+
+    it 'returns the correct records, when many records exist' do
+      item = create(:item) and item.destroy
+      item_two = create(:item)
+      expect(Item.deleted).to contain_exactly(item)
+    end
+  end
 end
