0afb494663c433ad6b3223357763cf58bb6591f1
Ciaran <ciaranobrien@umass.edu>
2016-08-12 12:13:38 -0400

    Modified rental controller spec

--- spec/controllers/rentals_controller_spec.rb
+++ spec/controllers/rentals_controller_spec.rb
@@ -158,12 +158,12 @@ describe RentalsController do
 
   describe 'GET #transaction_detail' do
     it 'assigns a requested rental to @rental'do
-      get :transaction_detail, params: { id: @rental }
+      get :show, params: { id: @rental }
       expect(assigns[:rental]).to eq @rental
     end
 
     it 'all requested financial transactions should contain the same rental as @rental' do
-      get :transaction_detail, params: { id: @rental }
+      get :show, params: { id: @rental }
       expect(assigns[:financial_transactions].all?{|ft| ft.rental.id == @rental.id}).to be true
     end
   end
