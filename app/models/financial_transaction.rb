e7994d043a6b495f822ec902f0d169778ed39521
Tom Peck <tapeck@umass.edu>
2016-09-19 12:23:05 -0400

    simplify fin trans validations

--- app/models/financial_transaction.rb
+++ app/models/financial_transaction.rb
@@ -3,7 +3,5 @@ class FinancialTransaction < ActiveRecord::Base
   belongs_to :rental
   belongs_to :transactable, polymorphic: true
 
-  validates :rental_id, presence: true
-  validates :amount, presence: true
-  validates :adjustment, presence: true
+  validates :adjustment, :rental_id, :amount, presence: true
 end
