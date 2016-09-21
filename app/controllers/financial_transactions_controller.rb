954d0ed00577d2d23324eeab5185190a505fbe10
Tom Peck <tapeck@umass.edu>
2016-09-21 11:36:02 -0400

    adding finacial transaction from rental page, should work

--- app/controllers/financial_transactions_controller.rb
+++ app/controllers/financial_transactions_controller.rb
@@ -16,7 +16,8 @@ class FinancialTransactionsController < ApplicationController
   def new
     @financial_transaction = FinancialTransaction.new
     @financial_transaction.rental = Rental.find(params.require(:rental_id))
-    @financial_transaction.transactable = @financial_transaction.rental
+    @financial_transaction.transactable_type = params.require(:transactable_type)
+    @financial_transaction.transactable_id = params.require(:transactable_id)
     if !@financial_transaction.rental
       flash[:danger] = 'A rental has not been found for this financial transaction.'
     end
@@ -25,15 +26,15 @@ class FinancialTransactionsController < ApplicationController
   # GET /financial_transactions/1/edit
   def edit
     @financial_transaction.rental = Rental.find(params.require(:rental_id))
-    @financial_transaction.transactable = @financial_transaction.rental
+    @financial_transaction.transactable_type = params.require(:transactable_type)
+    @financial_transaction.transactable_id = params.require(:transactable_id)
   end
 
   # POST /financial_transactions
   def create
     @financial_transaction = FinancialTransaction.new(financial_transaction_params)
-
     if @financial_transaction.save
-      redirect_to rental_invoice_path(@rental), success: 'Financial transaction was successfully created.'
+      redirect_to rental_invoice_path(@financial_transaction.rental_id), success: 'Financial transaction was successfully created.'
     else
       render :new
     end
@@ -57,8 +58,7 @@ class FinancialTransactionsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def financial_transaction_params
-    binding.pry
     notes = params.permit(:payed_by, :payment_form).to_h.to_s
-    params.require(:financial_transaction).permit(:amount, :adjustment, :rental_id)
+    params.require(:financial_transaction).permit(:amount, :adjustment, :rental_id, :transactable_type, :transactable_id).merge(note_field: notes)
   end
 end
