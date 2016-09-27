f6501593a36de7097a5a26274089d6389d7e9461
Tom Peck <tapeck@umass.edu>
2016-09-27 15:27:29 -0400

    add new payment factory and model test

---
+++ spec/factories/payment.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :payment do
+    payment_type 0
+    contact_name "billy"
+    contact_email "billy@bill.net"
+    contact_phone "8606176147"
+  end
+end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'requires payment_type' do
+      expect(build :payment, payment_type: nil).not_to be_valid
+    end
+
+    it 'requires contact_name' do
+      expect(build :payment, contact_name: nil).not_to be_valid
+    end
+
+    it 'requires contact_email' do
+      expect(build :payment, contact_email: nil).not_to be_valid
+    end
+
+    it 'requires contact_phone' do
+      expect(build :payment, contact_phone: nil).not_to be_valid
+    end
+  end
 end
