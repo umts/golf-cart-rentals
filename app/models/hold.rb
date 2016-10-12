1c4fef2efb5c8b830a25c4728726e4091303cdac
Alfonso Merkl <anmerkl@gmail.com>
2016-10-12 16:34:30 -0400

    code

--- app/models/hold.rb
+++ app/models/hold.rb
@@ -10,6 +10,19 @@ class Hold < ActiveRecord::Base
   validates :hold_reason, :item_id, :item_type_id, :start_time, :end_time, presence: true
   validates :end_time, date: { after: :start_time, message: 'must be after start time' }
 
+  def check_conflicting_rentals
+    if Rental.all.any? { |r| r.item == item }
+      replace_rental
+    else
+      reserve_hold
+    end
+  end
+
+  def replace_rental
+    rental = Rental.all.find { |r| r.item == item }
+
+  end
+
   def reserve_hold
     Inventory.update_item(item.uuid, start_time: start_time, end_time: end_time, reservable: false)
   end
