dcd71ce2d1dde7493d511c5c61476c61f71c3c3d
Tom Peck <tapeck@umass.edu>
2016-10-14 16:22:42 -0400

    remove stuff from incidental type

--- app/models/incidental_type.rb
+++ app/models/incidental_type.rb
@@ -2,7 +2,7 @@
 class IncidentalType < ActiveRecord::Base
   has_many :incurred_incidentals, dependent: :destroy
 
-  validates :name, :description, :base, :modifier_amount, :modifier_description, presence: true
+  validates :name, :description, :base, presence: true
   validates :name, uniqueness: true
-  validates :base, :modifier_amount, numericality: { greater_than_or_equal_to: 0 }
+  validates :base, numericality: { greater_than_or_equal_to: 0 }
 end
