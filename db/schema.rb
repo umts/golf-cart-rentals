8584351b76a3419bb9536e58fc18888d861a6258
Alfonso Merkl <anmerkl@gmail.com>
2016-10-14 16:16:56 -0400

    left over changes from migrations and running specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160926165113) do
 
   create_table "departments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
