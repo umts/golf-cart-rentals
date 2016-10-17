7def1531cdd2b4df7e6202f17ffdaf33b7312b49
Tom Peck <tapeck@umass.edu>
2016-10-17 11:20:26 -0400

    permissions inactive

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 class User < ActiveRecord::Base
   has_paper_trail
 
-  has_many   :groups_users, dependent: :destroy
+  has_many   :groups_users
   has_many   :groups, through: :groups_users
   has_many   :permissions, -> { distinct }, through: :groups
   has_many   :rentals
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   def has_permission?(controller, action, id = nil)
     return false if !active # inactive users shouldnt be able to do anything
     # Get a list of permissions associated with this controller and action
-    relevant_permissions = permissions.where(controller: controller, action: action)
+    relevant_permissions = permissions.where(controller: controller, action: action, active: true)
 
     # Deny if the list is empty
     return false if relevant_permissions.empty?
