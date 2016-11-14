79028ebb1a032c2409575cdbb0587de24e848d1d
Tom Peck <tapeck@umass.edu>
2016-11-14 12:36:39 -0500

    replace back in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102131612) do
+ActiveRecord::Schema.define(version: 20161021171019) do
 
   create_table "departments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                      null: false
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20161102131612) do
     t.string   "filename",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "fee_schedules", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.float    "base_amount",    limit: 24
+    t.float    "amount_per_day", limit: 24
+    t.integer  "item_type_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "financial_transactions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -95,10 +103,12 @@ ActiveRecord::Schema.define(version: 20161102131612) do
   end
 
   create_table "incurred_incidentals_documents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "incurred_incidental_id", null: false
-    t.integer  "document_id",            null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer  "incurred_incidental_id",               null: false
+    t.integer  "document_id",                          null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "filename"
+    t.binary   "file",                   limit: 65535
     t.index ["document_id"], name: "index_incurred_incidentals_documents_on_document_id", using: :btree
     t.index ["incurred_incidental_id", "document_id"], name: "index_on_incidentals_documents_id", unique: true, using: :btree
     t.index ["incurred_incidental_id"], name: "index_incurred_incidentals_documents_on_incurred_incidental_id", using: :btree
@@ -158,7 +168,7 @@ ActiveRecord::Schema.define(version: 20161102131612) do
     t.string   "rental_status",        null: false
     t.integer  "user_id",              null: false
     t.integer  "department_id",        null: false
-    t.string   "reservation_id"
+    t.string   "reservation_id",       null: false
     t.integer  "item_type_id",         null: false
     t.datetime "dropped_off_at"
     t.datetime "picked_up_at"
@@ -213,6 +223,8 @@ ActiveRecord::Schema.define(version: 20161102131612) do
   add_foreign_key "groups_permissions", "permissions", name: "fk_groups_permissions_permissions"
   add_foreign_key "groups_users", "groups", name: "fk_groups_users_groups"
   add_foreign_key "groups_users", "users", name: "fk_groups_users_users"
+  add_foreign_key "incurred_incidentals", "incidental_types"
+  add_foreign_key "incurred_incidentals", "rentals"
   add_foreign_key "incurred_incidentals_documents", "documents", name: "fk_incurred_incidentals_documents_documents"
   add_foreign_key "incurred_incidentals_documents", "incurred_incidentals", name: "fk_incurred_incidentals_documents_incurred_incidentals"
 end
