39a7a7ee76605be67e69b7384a947678d8dde08b
Alfonso Merkl <anmerkl@gmail.com>
2016-11-28 10:34:01 -0500

    minor updates for mailers

--- app/mailers/replacement_mailer.rb
+++ app/mailers/replacement_mailer.rb
@@ -1,18 +1,16 @@
 class ReplacementMailer < ActionMailer::Base
   def replacement_email(user, hold, old_rental, new_rental)
-    @user = user
     @hold = hold
     @old_rental = old_rental
     @new_rental = new_rental
 
-    mail(to: @user.email, subject: 'Your reserved Golf cart has been replaced')
+    mail(to: @user.email, subject: 'Parking Services: Replacement of reserved Golf Cart(s)')
   end
 
   def no_replacement_email(user, hold, rental)
-    @user = user
     @hold = hold
     @rental = rental
 
-    mail(to: @user.email, subject: 'Your reserved Golf cart has been cancelled')
+    mail(to: @user.email, subject: 'Parking Services: Cancellation of reserved Golf Cart(s)')
   end
 end
