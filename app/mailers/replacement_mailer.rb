68d7ae4bbb1b22cb76ea63fff7944f533d3f4264
Alfonso Merkl <anmerkl@gmail.com>
2016-11-28 15:55:49 -0500

    rubocop suggested changes

--- app/mailers/replacement_mailer.rb
+++ app/mailers/replacement_mailer.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
 class ReplacementMailer < ActionMailer::Base
   def replacement_email(user, hold, old_rental, new_rental)
     @hold = hold
     @old_rental = old_rental
     @new_rental = new_rental
 
-    mail(to: @user.email, subject: 'Parking Services: Replacement of reserved Golf Cart(s)')
+    mail(to: user.email, subject: 'Parking Services: Replacement of reserved Golf Cart(s)')
   end
 
   def no_replacement_email(user, hold, rental)
     @hold = hold
     @rental = rental
 
-    mail(to: @user.email, subject: 'Parking Services: Cancellation of reserved Golf Cart(s)')
+    mail(to: user.email, subject: 'Parking Services: Cancellation of reserved Golf Cart(s)')
   end
 end
