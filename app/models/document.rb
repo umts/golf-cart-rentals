d37906eb7b6bd748cd07a01f3680a3c1726f3132
Tom Peck <tapeck@umass.edu>
2016-12-01 15:09:07 -0500

    change relationshiips

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Document < ActiveRecord::Base
   validates :filename, presence: true
+  belongs_to :documentable, polymorphic: true
 
   def file
     "#{Rails.root}/storage/#{Rails.env}/#{filename}"
