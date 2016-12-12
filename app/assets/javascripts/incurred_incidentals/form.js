888fe998cbe65c4d1b509f5f14fc454e72062903
Tom Peck <tapeck@umass.edu>
2016-12-12 16:55:58 -0500

    removing a field, and pull it out into a partial

--- app/assets/javascripts/incurred_incidentals/form.js
+++ app/assets/javascripts/incurred_incidentals/form.js
@@ -1,15 +1,27 @@
 $(document).ready(function () {
-  $("#multiImageMoreFields").click(function () {
-    $(".multi-upload-group").first().clone().appendTo("#multiImageUploadContainer"); //add new group
+  $("#multiUploadMoreFields").click(function () {
+    if($(".multi-upload-group:visible").length < 1) {
+      $(".multi-upload-group").show(); //we never deleted it, just hid it
+    } else {
+      $(".multi-upload-group").first().clone().appendTo("#multiUploadContainer"); //add new group
 
-    //reset id's
-    var num = $(".multi-upload-group").length;
-    var group = $(".multi-upload-group").last(); //the grouping we just created
+      //reset id's
+      var num = $(".multi-upload-group").length;
+      var group = $(".multi-upload-group").last(); //the grouping we just created
 
-    var fileInput = group.find('.multi-upload-file'); //find file input in the group
-    fileInput.attr("name",fileInput.attr("name").replace(/\d/,num)); //change the number in the id
+      var fileInput = group.find('.multi-upload-file'); //find file input in the group
+      fileInput.attr("name",fileInput.attr("name").replace(/\d/,num)); //change the number in the id
 
-    var descInput = group.find('.multi-upload-desc'); //find file desc in the group
-    descInput.attr("name",descInput.attr("name").replace(/\d/,num)); //change the number in the id
+      var descInput = group.find('.multi-upload-desc'); //find file desc in the group
+      descInput.attr("name",descInput.attr("name").replace(/\d/,num)); //change the number in the id
+    }
+  });
+
+  $(".multiUploadDeleteField").click(function (e) {
+    if($(".multi-upload-group").length > 1) {
+      e.currentTarget.parentElement.remove();
+    } else {
+      $(e.currentTarget.parentElement).hide(); //dont remove it, just hide it
+    }
   });
 });
