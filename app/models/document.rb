b462b66e152501fb40f5bf7ffbfd701cb3b242c1
Tom Peck <tapeck@umass.edu>
2016-12-13 14:14:04 -0500

    use syntaxic sugar and define factory

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,16 +3,26 @@ include SecureRandom
 class Document < ActiveRecord::Base
   belongs_to :documentable, polymorphic: true
   enum filetype: %i(picture other)
-  before_create :write_file
+  validates :filename, :filetype, presence: true
+  before_validation :write_file
 
   attr_readonly :filename # this will be set in the before action
   attr_accessor :uploaded_file # dummy temp field to hold file
+
+  def fetch_file
+  end
 
   private 
     def write_file
       return unless uploaded_file
       begin 
         self.filename = SecureRandom.uuid
+        if uploaded_file.content_type.starts_with? "image/"
+          self.filetype = :picture
+        else
+          self.filetype = :other
+        end
+
         File.open(Rails.root.join('storage', Rails.env.to_s, self.filename), 'wb') do |file|
           file.write(self.uploaded_file.read)
         end
