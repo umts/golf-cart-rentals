aa94e54006095cfd18ebcde6e4009d42b4babd98
Tom Peck <tapeck@umass.edu>
2016-12-14 13:09:29 -0500

    document model specs

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -2,17 +2,40 @@
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
-  it 'has a valid factory' do
-    expect(build(:document)).to be_valid
-  end
-  it 'is invalid without a filename' do
-    expect(build(:document, filename: nil)).not_to be_valid
+  let(:document) { create(:document) } # creates document with png
+
+  context 'validations' do
+    it 'has a valid factory' do
+      expect(build(:document)).to be_valid
+    end
+    
+    it 'accepts a description' do
+      expect(build(:document, description: 'a special document')).to be_valid
+    end
   end
 
-  describe '#file' do
-    it 'returns a human readable string of the filepath' do
-      d = create(:document)
-      expect(d.file).to be_a(String)
+  context 'variable propogation' do
+    it 'sets original_filename' do
+      expect(document.original_filename).to be_present
     end
+
+    it 'sets filename as the file on disk' do
+      expect(document.filename).to be_present
+    end
+
+    it 'sets filetype' do
+      expect(document.filetype).to be_present
+      expect(document).to be_picture # based on the fixture in spec/fixtures/file.png
+    end
+  end
+
+  context 'retreiving file' do
+    it 'can retrieve file bytes' do
+      expect(document.fetch_file.bytes).to be_present
+    end
+
+    it 'can retreive as base64' do
+      expect(document.fetch_file_as_base64).to be_present
+    end 
   end
 end
