a0fc49649265213ee851a0790295ac7f17a1f87a
Tom Peck <tapeck@umass.edu>
2016-12-14 13:20:56 -0500

    add another fixture type and test for it

--- spec/factories/document.rb
+++ spec/factories/document.rb
@@ -3,5 +3,12 @@ include ActionDispatch::TestProcess
 FactoryGirl.define do
   factory :document do
     uploaded_file {fixture_file_upload('spec/fixtures/file.png', 'image/png')}
+    trait :with_invalid_file do
+      uploaded_file 'thing'
+    end
+
+    trait :with_text_file do
+      uploaded_file {fixture_file_upload('spec/fixtures/file.txt', 'text/plain')}
+    end
   end
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -23,9 +23,24 @@ RSpec.describe Document, type: :model do
       expect(document.filename).to be_present
     end
 
-    it 'sets filetype' do
-      expect(document.filetype).to be_present
-      expect(document).to be_picture # based on the fixture in spec/fixtures/file.png
+    context 'sets filetype' do
+      it 'sets a filetype' do
+        expect(document.filetype).to be_present
+      end
+
+      it 'sets picture' do
+        expect(document).to be_picture # based on the fixture in spec/fixtures/file.png
+      end
+
+      it 'sets other' do
+        expect(create(:document,:with_text_file)).to be_other # based on the fixture in spec/fixtures/file.txt
+      end
+    end
+  end
+
+  context 'refusing to write files' do
+    it 'can refuse and throw exception' do
+      expect{create(:document, :with_invalid_file)}.to raise_error
     end
   end
 
