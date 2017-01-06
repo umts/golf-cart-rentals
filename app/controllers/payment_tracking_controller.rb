7b09481a68a9c3ba80a44a278068e8b2ae03ffe0
Tom Peck <tapeck@umass.edu>
2017-01-06 16:53:45 -0500

    working on it

--- app/controllers/payment_tracking_controller.rb
+++ app/controllers/payment_tracking_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
-class PaymentTracking < ApplicationController
+class PaymentTrackingController < ApplicationController
 
   def index
     # collect unpaid rentals
-    #@rentals = Rentals.
+    @rentals = Rental
   end
 
 end
