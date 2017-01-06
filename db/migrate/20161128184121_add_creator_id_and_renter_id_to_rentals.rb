d49efee59a50cce15fafdb495709d44cb7248a50
Tom Peck <tapeck@umass.edu>
2017-01-06 10:01:48 -0500

    remove column and give type

--- db/migrate/20161128184121_add_creator_id_and_renter_id_to_rentals.rb
+++ db/migrate/20161128184121_add_creator_id_and_renter_id_to_rentals.rb
@@ -2,6 +2,6 @@ class AddCreatorIdAndRenterIdToRentals < ActiveRecord::Migration[5.0]
   def change
     add_column :rentals, :creator_id, :integer
     add_column :rentals, :renter_id, :integer
-    remove_column :rentals, :user_id # no longer needed
+    remove_column :rentals, :user_id, :integer # no longer needed
   end
 end
