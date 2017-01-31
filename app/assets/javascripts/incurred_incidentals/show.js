78ca79b372cd56a7bc26eb29721d9115e4450f97
Tom Peck <tapeck@umass.edu>
2017-01-31 16:47:33 -0500

    just the beginnings

--- app/assets/javascripts/incurred_incidentals/show.js
+++ app/assets/javascripts/incurred_incidentals/show.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-
+  $(".image-expand-action").click(function (e) {
+    e.preventDefault();
+    var modal = $("#modalIncurredIncidentalDocument");
+    modal.modal("toggle");
+    var image = $(this).find("img").clone(); //get the image
+    image.css(""); //strip the css which would make it a small image
+    modal.find();
+  });
 });
