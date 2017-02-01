4804aed9631b99b4f0ae5f7eb354195aafb83cdc
Tom Peck <tapeck@umass.edu>
2017-02-01 13:07:49 -0500

    gets desc and writes to title

--- app/assets/javascripts/incurred_incidentals/show.js
+++ app/assets/javascripts/incurred_incidentals/show.js
@@ -2,10 +2,13 @@ $(document).ready(function () {
   $(".image-expand-action").click(function (e) {
     e.preventDefault();
     var modal = $("#modalIncurredIncidentalDocument");
-    modal.modal("toggle");
+    modal.modal("toggle"); //open modal
+
     var image = $(this).find("img").clone(); //get the image
-    var desc = 
     image.css(""); //strip the css which would make it a small image
-    modal.find();
+    var doc = $(this).parent().parent(); //find entire document element
+    var desc = doc.find(".document-description").html().trim(); //get description from document
+
+    modal.find(".modal-title").html(desc); //write the desc into the modal title
   });
 });
