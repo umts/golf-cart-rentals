316ac8cb83353a3a8f9fa522666e278e1ecd5fd2
Tom Peck <tapeck@umass.edu>
2017-02-07 12:17:55 -0500

    dont use unnessesary paranethesis

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -157,7 +157,7 @@ class RentalsController < ApplicationController
   # Only allow a trusted parameter "white list" through.
   def rental_params
     # tokeninput gives us an array, but find_by doesnt care it just gives us the first one
-    user = User.find_by id: (params.require(:rental).require(:renter_id))
+    user = User.find_by id: params.require(:rental).require(:renter_id)
     new_time = Time.zone.parse(params[:rental][:end_time]).end_of_day
     params.require(:rental).permit(:start_time, :item_type_id, :pickup_name, :dropoff_name,
                                    :pickup_phone_number, :dropoff_phone_number).merge(renter: user, department_id: user.try(:department_id), end_time: new_time)
