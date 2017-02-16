1d21450938824a230342b13088c9abf8bab69be4
Tom Peck <tapeck@umass.edu>
2017-02-16 15:48:20 -0500

    umask

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,9 @@ set :application, 'probable-engine'
 set :repo_url, 'git@github.com:umts/probable-engine.git'
 set :deploy_to, '/var/www/probable-engine'
 set :rvm_type, :system
-
 set :linked_files, %w{config/database.yml config/config.yml config/secrets.yml}
+
+SSHKit.config.umask = '002'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
