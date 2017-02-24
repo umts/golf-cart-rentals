3b3b76ce080ef03653d37fdf8836f10346e2d920
Tom Peck <tapeck@umass.edu>
2017-02-24 13:38:26 -0500

    after deploy remove temp files

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,48 +6,10 @@ set :repo_url, 'git@github.com:umts/probable-engine.git'
 set :deploy_to, '/var/www/probable-engine'
 set :rvm_type, :system
 set :linked_files, %w{config/database.yml config/config.yml config/secrets.yml}
-
 SSHKit.config.umask = '002'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  # i think this is automatic now
-  #after :restart, :clear_cache do
-    #on roles(:web), in: :groups, limit: 3, wait: 10 do
-      #within current_path do
-        #execute :bundle, :exec, "rake db:migrate RAILS_ENV=production"
-      #end
-    #end
-  #end
-
+after :deploy, :finished do
+  on roles(:web) do
+    execute :rm, "-r /tmp/probable-engine/"
+  end
 end
