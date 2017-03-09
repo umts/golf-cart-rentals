5a00edb2a8cc1994042060f775d16be9dbf08105
Tom Peck <tapeck@umass.edu>
2017-03-09 14:18:38 -0500

    restart after deploy and seed perms too

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,3 +9,14 @@ set :linked_files, %w{config/database.yml config/config.yml config/secrets.yml c
 SSHKit.config.umask = '002'
 remote_user = Net::SSH::Config.for('umaps-web2.oit.umass.edu')[:user] || ENV['USER']
 set :tmp_dir, "/tmp/#{remote_user}"
+
+namespace :deploy do
+  task :restart do
+    on roles(:web), in: :sequence, wait: 5 do
+      execute :touch, release_path.join("tmp/restart.txt")
+      within current_path do
+        execute :bundle, :exec, "rake permissions:update RAILS_ENV=production"
+      end
+    end
+  end
+end
