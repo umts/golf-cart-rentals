2ad3696774a464597fddce1874bb169d3bfa5985
Tom Peck <tapeck@umass.edu>
2017-04-11 13:04:21 -0400

    extend flash with this helpful method

--- app/controllers/holds_controller.rb
+++ app/controllers/holds_controller.rb
@@ -20,11 +20,7 @@ class HoldsController < ApplicationController
       flash[:success] = 'Hold Successfully Created'
       @hold.handle_conflicting_rentals
 
-      # rental that has already started and conflicts with hold, needs to be handled
-      # by a csr
-      if rental = @hold.conflicting_ongoing_rental
-        flash_message :warning, "Ongoing rental with item #{url_for rental}".html_safe
-      end
+      ongoing_conflicts
       redirect_to @hold
     else
       @hold.errors.full_messages.each { |e| flash_message :warning, e, :now }
@@ -39,11 +35,7 @@ class HoldsController < ApplicationController
       flash[:success] = 'Hold Successfully Updated'
       @hold.handle_conflicting_rentals
 
-      # rental that has already started and conflicts with hold, needs to be handled
-      # by a csr
-      if rental = @hold.conflicting_ongoing_rental
-        flash_message :warning, "Ongoing rental with item #{url_for rental}".html_safe
-      end
+      ongoing_conflicts
       redirect_to @hold
     else
       @hold.errors.full_messages.each { |e| flash_message :warning, e, :now }
@@ -78,5 +70,13 @@ class HoldsController < ApplicationController
     damage = Damage.find_by id: params.fetch(:damage_id, nil)
     params.require(:hold).permit(:hold_reason, :item_id,
                                  :start_time, :end_time).merge(item_type_id: item_type_id, damage: damage)
+  end
+
+  def ongoing_conflicts
+    # rental that has already started and conflicts with hold,
+    # needs to be handled by a csr
+    if rental = @hold.conflicting_ongoing_rental
+      flash[:warning] = "Ongoing #{view_context.link_to 'rental', rental} with item".html_safe
+    end
   end
 end
---
+++ lib/flash.rb
@@ -0,0 +1,5 @@
+module ActionDispatch
+  class Flash
+    attr_accessor :html_safe?
+  end
+end
