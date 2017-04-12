59341e7f5790aeeba4bb311f70aea0eb0827f847
Cobchise <ciaranobrien@umass.edu>
2017-04-12 16:14:23 -0400

    Auto incidental on late dropoff

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -69,6 +69,10 @@ class Rental < ActiveRecord::Base
     event :drop_off do
       transitions from: :picked_up, to: :dropped_off
       after do
+        if end_time < Time.zone.now
+          note = Note.create note: 'Automatic incidental for late dropoff', noteable_type: 'IncurredIncidental'
+          incidental = IncurredIncidental.create incidental_type_id: 1, amount: 10, rental_id: id, notes: [note]
+        end
         update(dropped_off_at: Time.zone.now)
       end
     end
