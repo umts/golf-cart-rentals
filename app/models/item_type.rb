8f79d997ef03640eb2d3b81f20308a4104d79f71
Tom Peck <tapeck@umass.edu>
2017-04-18 12:30:59 -0400

    removed from model, and model tests

--- app/models/item_type.rb
+++ app/models/item_type.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class ItemType < ActiveRecord::Base
-  validates :name, :disclaimer, :base_fee, :fee_per_day, presence: true
+  validates :name, :base_fee, :fee_per_day, presence: true
   validates :base_fee, :fee_per_day, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :item
--- spec/models/item_type_spec.rb
+++ spec/models/item_type_spec.rb
@@ -14,9 +14,6 @@ RSpec.describe ItemType, type: :model do
   it 'is invalid without a fee_per_day' do
     expect(build(:item_type, fee_per_day: nil)).not_to be_valid
   end
-  it 'is invalid without a disclaimer' do
-    expect(build(:item_type, disclaimer: '')).not_to be_valid
-  end
   it 'is invalid with a negative base_fee' do
     expect(build(:item_type, base_fee: -1)).not_to be_valid
   end
