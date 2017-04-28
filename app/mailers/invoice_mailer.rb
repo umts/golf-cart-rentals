578a9429234cf5ceb639d0c747b2982ee96abddb
Tom Peck <tapeck@umass.edu>
2017-04-28 14:20:36 -0400

    add as attachment on email

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -2,6 +2,7 @@
 class InvoiceMailer < ActionMailer::Base
   def send_invoice(rental)
     @rental = rental
+    attachments['Recharge V04.pdf'] = File.read("#{Rails.root}/app/assets/Recharge V04.pdf")
     mail(to: rental.renter.email, subject: "Invoice for Rental #{rental.basic_info}", template_name: 'invoice')
   end
 end
