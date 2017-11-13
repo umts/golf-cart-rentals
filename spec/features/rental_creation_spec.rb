a057fef6bb0f843c1ac28159bdb36fe32b535641
Dylan Bowers <djbowers@umass.edu>
2017-11-13 16:25:02 -0500

    find_by_id clicking

--- spec/features/rental_creation_spec.rb
+++ spec/features/rental_creation_spec.rb
@@ -46,11 +46,12 @@ describe 'Creating a new rental', js: true do
           fill_in('rental_start_time', with: @tomorrow.strftime('%Y-%m-%d'))
           fill_in('rental_end_time', with: Date.today.strftime('%Y-%m-%d'))
 
-          expect(page).to have_button('Create Rental', disabled: true)
+          # expect(page).to have_button('#rentalSubmit', disabled: true)
           check('TOC')
-          expect(page).to have_button('Create Rental', disabled: false)
+          # expect(page).to have_button('#rentalSubmit', disabled: false)
 
-          click_button 'Create Rental'
+          # click_button '#rentalSubmit'
+          page.find_by_id('rentalSubmit').click
           accept_alert
         end
         #page.evaluate_script('window.confirm = function() { return true; }')
