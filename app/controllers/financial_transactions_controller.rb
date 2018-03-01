088cbad6437243eaecb45a358245d2004da2ae72
Alfonso Merkl <anmerkl@gmail.com>
2018-03-01 09:59:09 -0500

    removed unused show/edit/update controller actions/views/routes. fixed filter on index page

--- app/controllers/financial_transactions_controller.rb
+++ app/controllers/financial_transactions_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 class FinancialTransactionsController < ApplicationController
-  before_action :set_financial_transaction, only: [:show, :edit]
-
   after_action :set_return_url, only: [:index]
 
-  def show
-  end
-
   def index
-    @q = FinancialTransaction.search(params[:q])
+    # workaround for unknown Ransack bug
+    if params[:q]
+      @q = FinancialTransaction.search(transactable_type_eq: params[:q][:transactable_type])
+    else
+      @q = FinancialTransaction.search(nil)
+    end
     @trans_type = FinancialTransaction.all.pluck(:transactable_type).uniq
     @financial_transactions = @q.result.paginate(page: params[:page], per_page: 10)
   end
@@ -19,38 +19,32 @@ class FinancialTransactionsController < ApplicationController
     @financial_transaction.transactable_type = params[:transactable_type]
 
     # handles transactable_type payment which will be created with this form
-    @financial_transaction.transactable_id = params[:transactable_id] if @financial_transaction.transactable_type != Payment.name
+    if @financial_transaction.transactable_type != Payment.name
+      @financial_transaction.transactable_id = params[:transactable_id]
+    end
   end
 
   def create
     @financial_transaction = FinancialTransaction.new(financial_transaction_params)
     if @financial_transaction.transactable_type == Payment.name
-      payment = Payment.new(payment_params) # hard fail
+      payment = Payment.new(payment_params)
       unless payment.save
         flash[:danger] = 'Please Properly Fill Out Contact And Payment Fields'
-        render(:new) && return
+        render :new and return
       end
+
       @financial_transaction.transactable_id = payment.id
     end
 
     if @financial_transaction.save
-      redirect_to rental_invoice_path(@financial_transaction.rental_id), success: 'Financial Transaction Successfully Created'
+      redirect_to rental_invoice_path(@financial_transaction.rental_id),
+                  success: 'Financial Transaction Successfully Created'
     else
       render :new
     end
   end
 
-  def edit
-  end
-
-  def update
-  end
-
   private
-
-  def set_financial_transaction
-    @financial_transaction = FinancialTransaction.find(params[:id])
-  end
 
   def financial_transaction_params
     params.require(:financial_transaction).permit(:amount, :rental_id, :transactable_type, :transactable_id)
