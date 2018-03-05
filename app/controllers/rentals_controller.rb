f8dbffd15fc754c55f60e6d46ca92d8d7d5dd97e
Elisa Coutant <elisacoutant@comcast.net>
2018-03-05 13:49:24 -0500

    Changed so it shows only number of available carts of each type

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -70,6 +70,16 @@ class RentalsController < ApplicationController
     @admin_status = @current_user.has_group? Group.where(name: 'admin')
 
     @reservable = Item.all_reservable_items
+    @count4 = 0
+    @count6 = 0
+
+    @reservable.each do |item|
+      if(item.item_type.name == "4 Seat")
+        @count4 = @count4 + 1
+      else
+        @count6 = @count6 + 1
+      end
+    end
 
     set_users_to_assign
   end
