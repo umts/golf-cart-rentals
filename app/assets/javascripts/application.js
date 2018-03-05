ae150ebedf162e91386714d29b3ac0129d7a4b13
Elisa Coutant <elisacoutant@comcast.net>
2018-03-05 14:23:44 -0500

    Fixed indentation. Removed all *'s.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,3 +34,11 @@ $(document).ready(function() {
 });
 
 $( ".datepicker" ).datetimepicker(); //datepicker initialization
+
+/* Adds a red asterix next to required fields */
+$(function(){
+  $("[required]").each(function(index, element){
+    $(element).parent().addClass("required");
+    $(element).parents("form").addClass("with-required");
+  });
+});
--- public/ui.js
+++
@@ -1,7 +0,0 @@
-/* Adds a red asterix next to required fields */
-$(function(){
-  $("[required]").each(function(index, element){
-    $(element).parent().addClass("required");
-    $(element).parents("form").addClass("with-required");
-  });
-});
