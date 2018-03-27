17d7e7e7f0495e3010893f518c0f20c542e11c0b
Alfonso Merkl <anmerkl@gmail.com>
2018-03-27 10:20:15 -0400

    rubocop changes

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,11 +1,20 @@
+# frozen_string_literal: true
 class HelpController < ApplicationController
   def show; end
+
   def schedule; end
+
   def new_item_type; end
+
   def new_item; end
+
   def new_incidental_type; end
+
   def new_hold; end
+
   def new_department; end
+
   def new_user; end
+
   def new_group; end
 end
