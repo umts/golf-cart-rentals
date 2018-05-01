ac52cf83c7c1c91e553061bd7caf6f9ff93c1abf
Anbranin <keichelman@gmail.com>
2018-05-01 16:13:57 -0400

    add some passing system tests

---
+++ spec/system/rental_creation_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'creating a new rental' do
+  before :each do
+    @tomorrow = Time.zone.tomorrow
+    @day_after = @tomorrow + 1.day
+  end
+
+  describe 'form autopopulation' do
+    before :each do
+      FactoryBot.create :user
+      visit '/rentals/new'
+      @today = Date.today. strftime('%Y-%m-%d')
+    end
+
+    it 'populates the fields with the correct data' do
+      expect(find_by_id('rental_start_time').value).to eql @today
+    end
+  end
+end
