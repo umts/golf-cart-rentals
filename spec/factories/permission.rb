7623b18330d4aa1fe57686fecd4fc3c38240b857
Anbranin <keichelman@gmail.com>
2019-01-31 15:30:01 -0500

    give an actual controller and action so that the has_permission method doesn't freak out

--- spec/factories/permission.rb
+++ spec/factories/permission.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 FactoryBot.define do
   factory :permission do
-    controller 'Controller'
-    sequence(:action) { |n| "Action #{n}" }
+    controller 'User'
+    action 'show'
   end
 end
