ee1245e57d3e66d237fefe013beb8f7e5105d599
Anbranin <keichelman@gmail.com>
2019-02-08 13:47:00 -0500

    can't define a has_many :through before the association is defined.
    also, can remove validations for any items with belongs_to because belongs_to validates presence by
    default for rails 5.2

--- app/models/damage.rb
+++ app/models/damage.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Damage < ApplicationRecord
-  has_one :item, through: :incurred_incidental
 
   belongs_to :incurred_incidental, dependent: :destroy
-  belongs_to :hold, dependent: :destroy
+  # cannot define a has_many through before the association is defined
+  has_one :item, through: :incurred_incidental
+  belongs_to :hold, dependent: :destroy, optional: true
 
   validates :location, :description, :occurred_on, :estimated_cost, :incurred_incidental_id, presence: true
   validates :incurred_incidental_id, uniqueness: true
