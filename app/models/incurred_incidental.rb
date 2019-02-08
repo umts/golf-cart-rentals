a0521139add599b8d8156b1bba469e49302c7a7c
Anbranin <keichelman@gmail.com>
2019-02-08 13:40:11 -0500

    add papertrail to make up for lack of permanent records

--- app/models/incurred_incidental.rb
+++ app/models/incurred_incidental.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class IncurredIncidental < ActiveRecord::Base
+  has_paper_trail
+
   belongs_to :rental
   belongs_to :item
   belongs_to :incidental_type
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class Item < ActiveRecord::Base
+  has_paper_trail
+
   belongs_to :item_type
 
   validates :name, :item_type_id, presence: true
