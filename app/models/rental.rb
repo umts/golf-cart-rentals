21e484356aad9a483826d9696b32198f7bea2cd7
Anbranin <keichelman@gmail.com>
2019-02-14 16:28:08 -0500

    RecordInvalid is NOT a thing

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -124,7 +124,7 @@ class Rental < ActiveRecord::Base
     created_reservations = []
     begin
       rentals_items.each do |ri|
-        raise RecordInvalid, 'Rental item is invalid' unless ri.valid? # check if the current rental item is valid
+        raise ActiveRecord::RecordInvalid, 'Rental item is invalid' unless ri.valid? # check if the current rental item is valid
 
         reservation = Inventory.create_reservation(ri.item_type.name, start_time, end_time)
         raise 'Reservation UUID was not present in response.' unless reservation[:uuid].present?
