9ba264b27146e81e0f43d929860bca34f74715f8
Anbranin <keichelman@gmail.com>
2019-02-22 17:10:42 -0500

    belongs_to validates presence by default

--- app/models/rentals_item.rb
+++ app/models/rentals_item.rb
@@ -8,7 +8,6 @@ class RentalsItem < ActiveRecord::Base
   # TODO: remove item_type from rentals_item.
   belongs_to :item_type
   # we dont validate item, that will be assigned by the rental
-  validates :rental, presence: true 
   validates :reservation_id, uniqueness: true, allow_nil: true
 
   alias_attribute :reservation, :reservation_id
