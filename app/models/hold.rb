c03a802a00a35b568dc84198421240759afe750c
Anbranin <keichelman@gmail.com>
2019-02-26 17:05:09 -0500

    that's how you do that join more simply.

--- app/models/hold.rb
+++ app/models/hold.rb
@@ -20,9 +20,10 @@ class Hold < ActiveRecord::Base
   def handle_conflicting_rentals
     # if rental falls between data range of our hold
     # only reserved but not picked up
-    conflicting_rentals = Rental.reserved.joins('INNER JOIN rentals_items ON rentals_items.rental_id = rentals.id')
+    conflicting_rentals = Rental.reserved.joins(:rentals_items)
                                 .where('start_time <= :hold_end_time AND end_time >= :hold_start_time AND rentals_items.item_id = :hold_item_id',
                                        hold_start_time: start_time, hold_end_time: end_time, hold_item_id: item.id)
+    # Rental.where('start_time <= ? AND end_time >= ?', end_time, start_time) also works
     conflicting_rentals.each { |r| replace_rental(r) } unless conflicting_rentals.empty?
     start_hold
   end
